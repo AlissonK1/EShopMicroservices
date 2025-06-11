50b106004a3cd8d7c00a94c4a9b1a9dbcdc2ccbb
Alisson Kauan <Alisson.kauan@unisys.com>
2025-06-11 13:44:51 -0300

    Resumed course and development:
    - Tested Catalog MicroService's CreateProduct and started configuring PostgreSQL inside the Docker Container, with the integration of MartenDb as ORM for easier reading, storage and other activities.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,10 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
-using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using MediatR;
-using System.Threading;
-using System.Threading.Tasks;
-
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
-public record CreateProductResult(Guid id);
+public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -26,11 +20,11 @@ internal class CreateProductCommandHandler
             Price = command.Price,
         };
 
-        //Etapas relacionadas a bancos de dads/ Volta e faer
+        //Etapas relacionadas a bancos de dads/ Volta e fazer
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
-        return new CreateProductResult(Guid.NewGuid());
-
-        throw new NotImplementedException();
+        return new CreateProductResult(product.Id);
     }
 }
 
