187423880135f2de5e2e54431d6b2e19ef5c3a20
Alisson Kauan <Alisson.kauan@unisys.com>
2025-06-20 17:39:21 -0300

    Implemented WebService CRUD functions for Catalog, Update, Delete, and GetAllProducts, GetProductById and GetProductByCategory

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Products.GetProductById;
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+public class GetProductByCategoryQueryHandler
+        (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsByCategoryQueryHandler.Handle called with {@query}", query);
+
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductsByCategoryResult(products);
+    }
+}
